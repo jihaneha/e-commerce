9fbd6dc4c7d15ee3e23c7ba903fcc36e66bd3fd4
jihaneha <98841225+jihaneha@users.noreply.github.com>
2022-07-18 16:21:45 +0200

    Add files via upload

--- article1.php
+++ article1.php
@@ -2,43 +2,40 @@
 include('common/head.php');
 include('common/menu.php');
 ?>
-<section class="d-flex justify-content-between m-3">
-  <div>
-    <h4>MATIÈRES, ENTRETIEN ET ORIGINE</h4>
-    <h6>MATIÈRES</h6>
+<section class="d-flex justify-content-between m-5 w- p-3 w-80 h-60">
+  <div class="m-3 w-50 p-3">
+    <h4 class="pb-3">MATIÈRES, ENTRETIEN ET ORIGINE</h4>
+    <h6 class="pb-1">MATIÈRES</h6>
     <p>
 Nous travaillons avec des programmes de suivi pour garantir le respect des normes de sécurité, de santé et de qualité de nos produits. 
 
 L’objectif de la norme Green to Wear 2.0 est de réduire l’impact environnemental de la production textile. Pour cela, nous avons développé le programme The List d’Inditex qui nous aide à garantir aussi bien la propreté des processus de production que la sécurité et la santé de nos vêtements. </p>
-<h6>EXTÉRIEUR</h6>
+<h6 class="pb-1">EXTÉRIEUR</h6>
 <p>55% lin · 45% viscose</p>
-<h6>DOUBLURE</h6>
+<h6 class="pb-1">DOUBLURE</h6>
 <p>100% viscose</p>
-<h6>ENTRETIEN</h6>
+<h6 class="pb-1">ENTRETIEN</h6>
 <p>Prendre soin de vos vêtements, c'est prendre soin de l'environnement.
 Les lavages à basse température et les programmes d'essorage doux sont plus délicats avec les vêtements, ils aident à conserver la couleur, la forme et la structure du tissu. Ils réduisent en même temps la consommation d'énergie qui est utilisée dans les processus d'entretien.</p>
-<h6>PROVENANCE</h6>
+<h6 class="pb-1">PROVENANCE</h6>
 <p>Nous travaillons avec nos fournisseurs, employés, syndicats et organismes internationaux pour développer une chaîne d'approvisionnement dans laquelle les droits de l'homme sont respectés et encouragés, tout en contribuant aux Objectifs de développement durable des Nations unies. 
 
 De plus, grâce à la collaboration continue avec nos fournisseurs, nous avons développé un programme de traçabilité qui nous permet de savoir où et comment nos vêtements ont été confectionnés.
 Fabriqué en Indonésie</p>
   </div>
-  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div id="carouselExampleIndicators" class="carousel slide w-50 p-3 m-3 " data-bs-ride="carousel" >
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-  <div class="carousel-inner">
+  <div class="carousel-inner w-100">
     <div class="carousel-item active">
-      <img src="images/pexels-chalo-garcia-5489730.jpg" class="d-block w-100" alt="...">
+      <img src="images/4.jpg" class="d-block w-100 " alt="...">
     </div>
     <div class="carousel-item">
-      <img src="images/pexels-chalo-garcia-5489750.jpg" class="d-block w-100" alt="...">
+      <img src="images/4.jpg" class="d-block w-100 " alt="...">
     </div>
-    <div class="carousel-item">
-      <img src="images/pexels-chalo-garcia-5489756.jpg" class="d-block w-100" alt="...">
-    </div>
+    
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -52,8 +49,8 @@ Fabriqué en Indonésie</p>
   
 
 
-  <div class="m-3">
-    <h4>ROBE À RAYURES CONTENANT DU LIN</h4>
+  <div class="m-3 w-50 p-3">
+    <h4 class="pb-3">ROBE À RAYURES CONTENANT DU LIN</h4>
     <p>Robe mi-longue confectionnée dans un tissu en mélange de lin. Col à revers, manches courtes et larges terminées par un revers avec patte boutonnée. Ceinture à boucle dorée. Poches latérales dissimulées dans la couture. Fermeture par boutons dorés sur le devant.
     </p>
     <p>BLEU / BLANC | 7969/056</p>
@@ -68,12 +65,12 @@ Fabriqué en Indonésie</p>
     </ul>
     <hr>
     <p>GUIDE DE TAILLES</p>
-      <button>AJOUTER AU PANIER</button>
+      <button class="p-2 m-3">AJOUTER AU PANIER</button>
     <p>VOIR DISPONIBILITÉ EN MAGASIN</p>
     <p>LIVRAISON, ÉCHANGE ET RETOURS</p>
   </div>
 </section>
-<h2 class="text-center m-5" >Tu pourrais aussi aimer</h2>
+<h2 class="text-center m-5 p-5" >Tu pourrais aussi aimer</h2>
 <div class="container">
   <div class="row">
     <div class="col">
